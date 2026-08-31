3ef595adad91343444f8f0d5ae80b55f124e9c05
Mridul Bhardwaj <mridul.2005.05@gmail.com>
2026-08-31 23:57:51 +0530

    app

---
+++ finguard/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import LandingPage from './components/LandingPage';
+import LoanRiskPredictor from './components/LoanRiskPredictor';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      {/* <Register /> */}
+      {/* <LandingPage /> */}
+      <LoanRiskPredictor />
+
+    </div>
+  );
+}
+
+export default App;
